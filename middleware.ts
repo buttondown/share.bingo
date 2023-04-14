9f9ebead5c01b1f3e56a18136da8e667112aeaa7
Justin Duke <justin@buttondown.email>
2023-04-14 11:31:35 -0400

    Do the thing

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 // middleware.ts
-import { rules } from "@/lib/rules";
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { rules } from "./lib/rules";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const url = searchParams.get("url");
+  const text = searchParams.get("text");
+
   for (const rule of rules) {
     if (request.nextUrl.pathname.startsWith(`/${rule.name}`)) {
-      const { url, text } = Object.fromEntries(
-        request.nextUrl.searchParams.entries()
-      );
-      const redirectUrl = rule.render(url, text);
-      return NextResponse.redirect(redirectUrl);
+      const redirectUrl = rule.render(url || "", text || "");
+      return Response.redirect(redirectUrl);
     }
   }
 }
