8313760175094a4a5affcdb3448ff6102a8525a6
Justin Duke <justin@buttondown.email>
2023-10-17 19:45:25 -0400

    Use a raw response to avoid clever URL parsing in prod, part 2

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export function middleware(request: NextRequest) {
       return new Response(null, {
         status: 302,
         headers: {
+          "Content-Location": "",
           Location: redirectUrl,
         },
       });
